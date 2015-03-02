ded32d43149d42d86cdbb64fd56fba5223efb6af
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-03-02 18:49:00 -0500

    Fixed flagging empty zeros

--- Minesweeper V2.js
+++ Minesweeper V2.js
@@ -1044,7 +1044,7 @@ $(document).keydown(function(event) {
   numMines = Number($("#numMines p").html());
   if(event.which == 32) {
     cont = $("#" + whereAmI + " p").html();
-    if(cont == "&nbsp;") {
+    if(cont == "&nbsp;" && count[whereAmI] != 0) {
       $("#" + whereAmI + " p").html("+").css("color", "red");
       numMines--;
     }
